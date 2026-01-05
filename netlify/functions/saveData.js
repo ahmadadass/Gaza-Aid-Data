52b2cd564448826d798bdfd8a8ce608e61f4217e
ahmad <adass0301@gmail.com>
2026-01-05 12:24:22 +0200

    adding logs

--- netlify/functions/saveData.js
+++ netlify/functions/saveData.js
@@ -1,6 +1,7 @@
 // netlify/functions/saveData.js
-export async function handler(event, context) {
+exports.handler = async (event) => {
   const body = JSON.parse(event.body);
+  console.log("body:",body);
   const response = await fetch('https://script.google.com/macros/s/AKfycbzqU9f5WBlb_u93SPP6BEGQvxOkpjHIOfUJwBzP_1hTjZOpd6ytCJWEmKd0uuFJwLUZ/exec', {
     method: 'POST',
     body: JSON.stringify(body),
